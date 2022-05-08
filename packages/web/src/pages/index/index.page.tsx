a8733732a9df0f0524fc5ae84c8aba9233efb2fa
RanolP <public.ranolp@gmail.com>
2022-05-08 21:50:34 +0900

    chore: fix link

--- packages/web/src/pages/index/index.page.tsx
+++ packages/web/src/pages/index/index.page.tsx
@@ -3,7 +3,7 @@ import { Link } from '../../renderer/Link';
 export function Page() {
   return (
     <>
-      <Link href="/posts/블로그를-만들기-위한-첫-걸음/edit">여기</Link>에서
+      <Link href="/posts/블로그를-만들기-위한-한-걸음/edit">여기</Link>에서
       에디터를 구경하세요 아직 아무것도 안 됨
     </>
   );
