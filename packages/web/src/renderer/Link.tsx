49da1bddcf1eeb14129c5b6e8e1597ff32cf4a9a
RanolP <public.ranolp@gmail.com>
2022-05-08 21:56:05 +0900

    fix: base url for <Link />

--- packages/web/src/renderer/Link.tsx
+++ packages/web/src/renderer/Link.tsx
@@ -1,6 +1,9 @@
 import { usePageContext } from './usePageContext';
 
 export function Link({ href, children }: { href: string; children: string }) {
+  if (!href.startsWith('/')) throw new Error('Link href should start with /');
+  href = href.slice(1); // We remove the leading '/'
+
   const pageContext = usePageContext();
   const className = [
     'navigation-link',
@@ -8,8 +11,9 @@ export function Link({ href, children }: { href: string; children: string }) {
   ]
     .filter(Boolean)
     .join(' ');
+
   return (
-    <a href={href} className={className}>
+    <a href={import.meta.env.BASE_URL + href} className={className}>
       {children}
     </a>
   );
